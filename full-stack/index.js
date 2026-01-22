c0be5dd7810fe0f2860bead9013aae9a3293ea5b
Deepak Pun <pun.deepak1986@gmail.com>
2026-01-22 10:35:02 -0500

    Fixed CSP to use domain name only instead of full urls

--- full-stack/index.js
+++ full-stack/index.js
@@ -79,19 +79,19 @@ app.use(helmet({
       styleSrc: [
         "'self'",
         "'unsafe-inline'",
-        "https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css",
-        "https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap",
-        "https://kit.fontawesome.com/51f52c5165.js"
+        "https://cdn.jsdelivr.net",
+        "https://fonts.googleapis.com",
+        "https://kit.fontawesome.com"
       ],
       scriptSrc: [
         "'self'",
-        "https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js",
+        "https://cdn.jsdelivr.net",
         // "https://kit.fontawesome.com"         // FontAwesome JS
       ],
       fontSrc: [
         "'self'",
-        "https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap",
-        "https://kit.fontawesome.com/51f52c5165.js"
+        "https://fonts.googleapis.com",
+        "https://kit.fontawesome.com"
       ],
       imgSrc: ["'self'", "data:", "https:"],
       connectSrc: ["'self'"]
